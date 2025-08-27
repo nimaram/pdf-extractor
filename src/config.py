2bc6d7f3f6a6b340d80bd0757c80a87559bb5575
nimaram <aramnima50@gmail.com>
2025-08-27 23:41:08 +0330

    fix(ci)

--- src/config.py
+++ src/config.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
 from pydantic import Field
+from typing import Optional
 import os
 
 # Loading environmental variables
@@ -10,8 +11,8 @@ load_dotenv()
 
 class Settings(BaseSettings):
     # TODO: move other setting variables to this class
-    gemini_url: str = Field(env="GEMINI_URL")
-    gemini_api_key: str = Field(env="GEMINI_API_KEY")
+    gemini_url: Optional[str] = Field(env="GEMINI_URL")
+    gemini_api_key: Optional[str] = Field(env="GEMINI_API_KEY")
 
 
 # Base directory(src)
